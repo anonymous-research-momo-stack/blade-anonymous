345c1e8ef5c18948cfe8cc1323c49742e069ccfd
LIU <liuchengyuechina@gmail.com>
2025-09-15 20:12:47 +0800

    remove useless code

--- debug.py
+++ debug.py
@@ -4,92 +4,8 @@ from app.services.tpl_detection.detection_workflow import DetectionWorkflow
 from app.services.tpl_detection.batch_detection_workflow import BatchDetectionWorkflow
 import time
 
-def debug_batch():
-    openssl_path = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/Benchmarks/FTPL100/decompressed_deb/openssl_3.0.2-0ubuntu1.18_amd64/usr/bin/openssl"
-    libpng16_so_path = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/Benchmarks/FTPL100/decompressed_deb/libpng16-16_1.6.37-3build5_amd64/usr/lib/x86_64-linux-gnu/libpng16.so.16"
-    ffmpeg_path = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/Benchmarks/FTPL100/decompressed_deb/ffmpeg_7%3a4.4.2-0ubuntu0.22.04.1_amd64/usr/bin/ffmpeg"
-
-    file_paths = [openssl_path, libpng16_so_path, ffmpeg_path]
-    batch_workflow = BatchDetectionWorkflow(concurrency=3, feature_matching_return_top_n=3)
-    start_time = time.time()
-    results = batch_workflow.run_batch(file_paths)
-    total_time = time.time() - start_time
-    print(f"\n总耗时: {total_time:.2f} 秒\n")
-
-    for idx, result in enumerate(results):
-        if result is not None:
-            save_path = f"/Users/liuchengyue/Desktop/BinarySCA Platform/Code/sca_agents/bsca-expert-agent-api/tmp/analysis_result_{idx}.json"
-            result.dump_to_file(save_path)
-            print(f"Result {idx} saved to {save_path}")
-            print(f"Result {idx} durations:")
-            print(result.analysis_data.durations)
-        else:
-            print(f"Result {idx} failed.")
-
-def demo():
-    openssl_path = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/Benchmarks/FTPL100/decompressed_deb/openssl_3.0.2-0ubuntu1.18_amd64/usr/bin/openssl"
-    libpng16_so_path = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/Benchmarks/FTPL100/decompressed_deb/libpng16-16_1.6.37-3build5_amd64/usr/lib/x86_64-linux-gnu/libpng16.so.16"
-    ffmpeg_path = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/Benchmarks/FTPL100/decompressed_deb/ffmpeg_7%3a4.4.2-0ubuntu0.22.04.1_amd64/usr/bin/ffmpeg"
-    libsqlite3_so_path = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/Benchmarks/FTPL100/decompressed_deb/libsqlite3-0_3.37.2-2ubuntu0.3_amd64/usr/lib/x86_64-linux-gnu/libsqlite3.so.0.8.6"
-    fftw3_path = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/Benchmarks/FTPL100/decompressed_deb/libfftw3-double3_3.3.8-2ubuntu8_amd64/usr/lib/x86_64-linux-gnu/libfftw3.so.3.5.8"
-    libpg_path = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/Benchmarks/FTPL100/decompressed_deb/libpq5_14.15-0ubuntu0.22.04.1_amd64/usr/lib/x86_64-linux-gnu/libpq.so.5.14"
-    libvulkan_path = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/Benchmarks/FTPL100/decompressed_deb/libvulkan1_1.3.204.1-2_amd64/usr/lib/x86_64-linux-gnu/libvulkan.so.1.3.204"
-
-    # Car cases
-    # mbedtls
-    secverify_path = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/BYD/update/mdm9607-boot.img.xx_/ramdisk.xx_/usr/bin/secverify"
-    libdevmapper_so_path = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/BYD/update/mdm9607-boot.img.xx_/ramdisk.xx_/usr/lib/libdevmapper.so.1.02"
-
-    # DDE 2000
-    libpulse_so = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/Benchmarks/DDE2000/libpulse.so"
-    sktest = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/Benchmarks/DDE2000/sktest" # 特征匹配不到
-    care = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/Benchmarks/DDE2000/care" # 特征匹配不到
-    libcpu_ldap = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/Benchmarks/DDE2000/libcpu_ldap.so.0.0.0"
-
-    # 根目录
-    root_path = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/BYD/"
-
-    # conan test cases
-    conan_bin_dir = "/Users/liuchengyue/Desktop/BinarySCA Platform/Code/sca_agents/bsca-expert-agent-api/evaluation/conan_libs_builder_output/"
-
-    grpc_cpp_plugin_shared = os.path.join(conan_bin_dir, "grpc/1.72.0/grpc_1.72.0_x86_64-gcc-release-shared/full_deploy/host/grpc/1.72.0/Release/x86_64/bin/grpc_cpp_plugin")
-    setfattr_static = os.path.join(conan_bin_dir, "acl/2.3.1/acl_2.3.1_x86_64-gcc-release-static/full_deploy/host/libattr/2.5.1/Release/x86_64/bin/setfattr")
-    setfacl_shared = os.path.join(conan_bin_dir, "acl/2.3.1/acl_2.3.1_arm_64-gcc-release-shared/full_deploy/host/acl/2.3.1/Release/armv8/bin/setfacl")
-    getfacl_static = os.path.join(conan_bin_dir, "acl/2.3.1/acl_2.3.1_x86_64-gcc-release-static/full_deploy/host/acl/2.3.1/Release/x86_64/bin/getfacl")
-    libacl_so = "/Users/liuchengyue/Desktop/BinarySCA Platform/Code/sca_agents/bsca-expert-agent-api/evaluation/conan_libs_builder_output/acl/2.3.1/acl_2.3.1_x86_64-clang-release-shared/full_deploy/host/acl/2.3.1/Release/x86_64/lib/libacl.so"
-    getfattr = "/Users/liuchengyue/Desktop/BinarySCA Platform/Code/sca_agents/bsca-expert-agent-api/evaluation/conan_libs_builder_output/acl/2.3.1/acl_2.3.1_x86_64-gcc-release-static/full_deploy/host/libattr/2.5.1/Release/x86_64/bin/getfattr"
-    libgrpc_so = "/Users/liuchengyue/Desktop/BinarySCA Platform/Code/sca_agents/bsca-expert-agent-api/evaluation/conan_libs_builder_output/grpc/1.72.0/grpc_1.72.0_x86_64-gcc-release-shared/full_deploy/host/grpc/1.72.0/Release/x86_64/lib/libgrpc.so"
-
-    grpc_cpp_plugin_static = os.path.join(conan_bin_dir,
-                                          "grpc/1.72.0/grpc_1.72.0_x86_64-gcc-release-static/full_deploy/host/grpc/1.72.0/Release/x86_64/bin/grpc_csharp_plugin")
-
-    libaaplus_so = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/conan_test_cases/aaplus/2.53/aaplus_2.53_arm_64-gcc-release-shared/full_deploy/host/aaplus/2.53/Release/armv8/lib/libaaplus.so"
-    imake = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/conan_test_cases/imake/1.0.9/imake_1.0.9_arm_64-gcc-release-shared/full_deploy/host/imake/1.0.9/Release/armv8/bin/imake"
-    boost = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/conan_test_cases/cgal/6.0.1/cgal_6.0.1_x86_64-gcc-release-shared/full_deploy/host/boost/1.83.0/Release/x86_64/lib/libboost_system.so.1.83.0"
-
-    libnsimd_cpu = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/conan_test_cases/nsimd/3.0.1/nsimd_3.0.1_arm_64-gcc-release-shared/full_deploy/host/nsimd/3.0.1/Release/armv8/lib/libnsimd_cpu.so"
-    libz_so = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/conan_test_cases/cgal/6.0.1/cgal_6.0.1_x86_64-clang-release-shared/full_deploy/host/zlib/1.3.1/Release/x86_64/lib/libz.so.1.3.1"
-    boost = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/conan_test_cases/cgal/6.0.1/cgal_6.0.1_x86_64-gcc-release-shared/full_deploy/host/boost/1.83.0/Release/x86_64/lib/libboost_system.so.1.83.0"
-    libPcap = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/conan_test_cases/pcapplusplus/25.05/pcapplusplus_25.05_x86_64-gcc-release-shared/full_deploy/host/pcapplusplus/25.05/Release/x86_64/lib/libPcap++.so.25.05"
-    libazure = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/conan_test_cases/azure-sdk-for-cpp/1.11.3/azure-sdk-for-cpp_1.11.3_x86_64-gcc-release-shared/full_deploy/host/azure-sdk-for-cpp/1.11.3/Release/x86_64/lib/libazure-core.so"
-    m4 = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/conan_test_cases/nanodbc/cci.20200807/nanodbc_cci.20200807_x86_64-gcc-release-shared/full_deploy/host/m4/1.4.19/Release/x86_64/bin/m4"
-    libohNet_so = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/conan_test_cases/ohnet/1.37.5454/ohnet_1.37.5454_x86_64-clang-release-shared/full_deploy/host/ohnet/1.37.5454/Release/x86_64/lib/libohNet.so"
-    clp = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/conan_test_cases/coin-cgl/0.60.7/coin-cgl_0.60.7_x86_64-clang-release-shared/full_deploy/host/coin-clp/1.17.7/Release/x86_64/bin/clp"
-    libboost_system = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/conan_test_cases/arg_router/1.3.0/arg_router_1.3.0_x86_64-clang-release-shared/full_deploy/host/boost/1.81.0/Release/x86_64/lib/libboost_system.so.1.81.0"
-    libuuid = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/conan_test_cases/bmx/cci.20240517/bmx_cci.20240517_x86_64-gcc-release-shared/full_deploy/host/libuuid/1.0.3/Release/x86_64/lib/libuuid.so.1.0.0"
-    libboost_system_182 = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/conan_test_cases/boost/1.88.0/boost_1.88.0_arm_64-gcc-release-shared/full_deploy/host/boost/1.88.0/Release/armv8/lib/libboost_system.so.1.88.0"
-    kcov = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/conan_test_cases/kcov/42/kcov_42_x86_64-gcc-release-shared/full_deploy/host/kcov/42/x86_64/bin/kcov"
-    libgrpc = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/conan_test_cases/etcd-cpp-apiv3/0.15.4/etcd-cpp-apiv3_0.15.4_x86_64-clang-release-shared/full_deploy/host/grpc/1.54.3/Release/x86_64/lib/libgrpc.so.31.0.0"
-    libtcl8 = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/conan_test_cases/tcl/8.6.13/tcl_8.6.13_x86_64-gcc-release-shared/full_deploy/host/tcl/8.6.13/Release/x86_64/lib/libtcl8.6.so"
-    libboost_system = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/conan_test_cases/svgpp/cci.20221030/svgpp_cci.20221030_arm_64-gcc-release-shared/full_deploy/host/boost/1.82.0/Release/armv8/lib/libboost_system.so.1.82.0"
-    libhuffman_so = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/conan_test_cases/huffman/1.2.7/huffman_1.2.7_x86_64-clang-release-shared/full_deploy/host/huffman/1.2.7/Release/x86_64/lib/libhuffman.so"
-    secverify = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/BYD/update/mdm9607-boot.img.xx_/ramdisk.xx_/usr/bin/secverify"
-    openssl = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/conan_test_cases/azure-sdk-for-cpp/1.11.3/azure-sdk-for-cpp_1.11.3_x86_64-clang-release-shared/full_deploy/host/openssl/3.1.1/Release/x86_64/bin/openssl"
-    libschrift = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/conan_test_cases/libschrift/0.10.2/libschrift_0.10.2_x86_64-clang-release-shared/full_deploy/host/libschrift/0.10.2/Release/x86_64/lib/libschrift.so"
-    libssl = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/conan_test_cases/libressl/3.9.1/libressl_3.9.1_x86_64-clang-release-shared/full_deploy/host/libressl/3.9.1/Release/x86_64/lib/libssl.so.56.0.0"
-    # "nsimd/3.0.1/nsimd_3.0.1_arm_64-gcc-release-shared/full_deploy/host/nsimd/3.0.1/Release/armv8/lib/libnsimd_cpu.so"
-
-    # 分析工作流
+def run_test_case(openssl_path, result_save_path):
+    # analysis workflow
     workflow = DetectionWorkflow(
         enable_bin_info_analysis_web_search=False,
         feature_matching_return_top_n=5,
@@ -99,20 +15,20 @@ def demo():
     )
 
 
-    # 分析
-    result = workflow.run(libuuid)
+    # run analysis
+    result = workflow.run(openssl_path)
 
-    # 结合上下文分析
-    # context = workflow.analyze_context(root_path)
-    # result = workflow.run(secverify_path, software_context=context)
-
-    # 预览分析结果
+    # preview analysis result
     result.analysis_data.preview()
 
-    result_save_path = "/Users/liuchengyue/Desktop/BinarySCA Platform/Code/sca_agents/bsca-expert-agent-api/tmp/analysis_result.json"
+    # dump result
     result.dump_to_file(result_save_path)
+
+def main():
+    openssl_path = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/Benchmarks/FTPL100/decompressed_deb/openssl_3.0.2-0ubuntu1.18_amd64/usr/bin/openssl"
+    result_save_path = "/Users/liuchengyue/Desktop/BinarySCA Platform/Code/sca_agents/bsca-expert-agent-api/tmp/analysis_result.json"
+    run_test_case(openssl_path, result_save_path)
 
 
 if __name__ == '__main__':
-    # debug_batch()
-    demo()
+    main()
--- evaluator_runner.py
+++ evaluator_runner.py
@@ -1,239 +1,11 @@
-import json
-import os
+from environs import Env
 
 from evaluation.general_benchmarks.evaluator import Evaluator
-from evaluation.general_benchmarks.feture_matching_analysis import plot_feature_matching_top_n_effectiveness_figure, \
-    analyze_feature_matching_top_n_effectiveness, \
-    analyze_feature_matching_failures, print_failure_analysis_report
-from evaluation.general_benchmarks.interface import EvaluationConfig, SimpleEvaluationReport
-from environs import Env
+from evaluation.general_benchmarks.interface import EvaluationConfig
 
 env = Env()
 env.read_env()
 
-
-def reanalyze_all_result():
-    # Conan Binaries
-    Conan_benchmark_meta = "/Users/liuchengyue/Desktop/BinarySCA Platform/Code/sca_agents/bsca-expert-agent-api/evaluation/general_benchmarks/benchmark_meta/conan_library_benchmark_20250831_2032.json"
-    Conan_test_case_dir = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/conan_test_cases"
-
-
-    benchmark_meta = Conan_benchmark_meta
-    benchmark_tc_dir = Conan_test_case_dir
-
-
-    # 评估配置
-    config = EvaluationConfig(
-        benchmark_file=benchmark_meta,
-        test_case_dir=benchmark_tc_dir,
-        concurrency=30,
-        slice_start=0,
-        slice_end=30,
-    )
-
-    # 初始化评估器
-    evaluator = Evaluator(config)
-    # =============== 重新对比结果与groundtruth ==============
-    print(f"our gpt 5 mini")
-    result_path = "/Users/liuchengyue/Desktop/BinarySCA Platform/Code/sca_agents/bsca-expert-agent-api/tmp/evaluation_reports/Conan/ours/gpt_5_mini/evaluation_report.json"
-    report = evaluator.reanalyze_report(result_path, ignore_failed_cases=False)
-
-    print(f"our gpt 5 nano")
-    result_path = "/Users/liuchengyue/Desktop/BinarySCA Platform/Code/sca_agents/bsca-expert-agent-api/tmp/evaluation_reports/Conan/ours/gpt_5_nano/evaluation_report.json"
-    report = evaluator.reanalyze_report(result_path, ignore_failed_cases=False)
-
-    print(f"our gpt 4.1 mini")
-    result_path = "/Users/liuchengyue/Desktop/BinarySCA Platform/Code/sca_agents/bsca-expert-agent-api/tmp/evaluation_reports/Conan/ours/gpt_4_1_mini/ours_105_0806/evaluation_report.json"
-    report = evaluator.reanalyze_report(result_path, ignore_failed_cases=False)
-
-    print(f"our qwen 3")
-    result_path = "/Users/liuchengyue/Desktop/BinarySCA Platform/Code/sca_agents/bsca-expert-agent-api/tmp/evaluation_reports/Conan/ours/qwen3/evaluation_report.json"
-    report = evaluator.reanalyze_report(result_path, ignore_failed_cases=False)
-
-    print(f"Blackduck")
-    result_path = "/Users/liuchengyue/Desktop/BinarySCA Platform/Code/sca_agents/bsca-expert-agent-api/tmp/evaluation_reports/Conan/blackduck/result_converted.json"
-    report = evaluator.reanalyze_report(result_path, ignore_failed_cases=False)
-
-    print(f"Scantist")
-    result_path = "/Users/liuchengyue/Desktop/BinarySCA Platform/Code/sca_agents/bsca-expert-agent-api/tmp/evaluation_reports/Conan/scantist/291-75403-xd70-无agent-扫描报告-2025-08-06T09_36_22+08_00/result_converted.json"
-    report = evaluator.reanalyze_report(result_path, ignore_failed_cases=False)
-
-    print(f"BAT")
-    result_path = "/Users/liuchengyue/Desktop/BinarySCA Platform/Code/sca_agents/bsca-expert-agent-api/tmp/evaluation_reports/Conan/bat/raw_result_converted.json"
-    report = evaluator.reanalyze_report(result_path, ignore_failed_cases=False)
-
-    print(f"OssPolice")
-    result_path = "/Users/liuchengyue/Desktop/BinarySCA Platform/Code/sca_agents/bsca-expert-agent-api/tmp/evaluation_reports/Conan/osspolice/raw_result_converted.json"
-    report = evaluator.reanalyze_report(result_path, ignore_failed_cases=False)
-
-    print(f"Binary AI")
-    result_path = "/Users/liuchengyue/Desktop/BinarySCA Platform/Code/sca_agents/bsca-expert-agent-api/tmp/evaluation_reports/Conan/binary_ai/evaluation_report_2025-07-30-12-54-29_converted.json"
-    report = evaluator.reanalyze_report(result_path, ignore_failed_cases=False)
-
-    print(f"B2SFinder")
-    result_path = "/Users/liuchengyue/Desktop/BinarySCA Platform/Code/sca_agents/bsca-expert-agent-api/tmp/evaluation_reports/Conan/b2sfinder/result_converted.json"
-    report = evaluator.reanalyze_report(result_path, ignore_failed_cases=False)
-
-
-
-def run_feature_matching_only():
-    # 分析输入Conan Binaries
-    Conan_benchmark_meta = "/Users/liuchengyue/Desktop/BinarySCA Platform/Code/sca_agents/bsca-expert-agent-api/evaluation/general_benchmarks/benchmark_meta/conan_library_benchmark_20250831_2032.json"
-    Conan_test_case_dir = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/conan_test_cases"
-    Conan_evluation_report_path = "/Users/liuchengyue/Desktop/BinarySCA Platform/Code/sca_agents/bsca-expert-agent-api/tmp/evaluation_reports/Conan/feture_matching/evaluation_report.json"
-
-    # 评估输入
-    benchmark_meta = Conan_benchmark_meta
-    benchmark_tc_dir = Conan_test_case_dir
-
-    # 评估结果
-    evaluation_report_save_path = Conan_evluation_report_path
-
-    # 评估结果的分析结果
-    top_n_effectiveness_analysis_result_path = "/Users/liuchengyue/Desktop/BinarySCA Platform/Code/sca_agents/bsca-expert-agent-api/tmp/evaluation_reports/Conan/feture_matching/effectiveness_analysis_result.json"
-
-    # 评估配置
-    top_n = 30
-    config = EvaluationConfig(
-        benchmark_file=benchmark_meta,
-        test_case_dir=benchmark_tc_dir,
-        feature_matching_top_n=top_n,
-        use_agent=False,
-        concurrency=10,
-        slice_start=0,
-        # slice_end=100,
-    )
-
-    # 初始化评估器
-    evaluator = Evaluator(config)
-
-    # 评估
-    # evaluator.run_benchmark(analyze_context=False)
-    # evaluator.report.dump(evaluation_report_save_path)
-
-    # 分析结果
-    # RQ 1 topn 和 效果的相关性
-    analyze_feature_matching_top_n_effectiveness(evaluator, evaluation_report_save_path, top_n_effectiveness_analysis_result_path, top_n=top_n)
-
-    # top_n_effectiveness 绘图
-    plot_feature_matching_top_n_effectiveness_figure(top_n_effectiveness_analysis_result_path)
-
-    # RQ 2 漏报的原因分类
-    # 执行分析
-    report = analyze_feature_matching_failures(
-        evaluator=evaluator,
-        benchmark_path=benchmark_meta,
-        evaluation_result_path=evaluation_report_save_path,
-        top_n=top_n,
-        small_file_threshold_kb=100
-    )
-
-    # 打印报告
-    print_failure_analysis_report(report)
-
-def run_failed_case_only():
-
-    # Conan Binaries
-    Conan_benchmark_meta_updated = "/Users/liuchengyue/Desktop/BinarySCA Platform/Code/sca_agents/bsca-expert-agent-api/evaluation/general_benchmarks/benchmark_meta/conan_library_benchmark_20250830_1142.json"
-    Conan_test_case_dir = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/conan_test_cases"
-
-    Conan_evluation_report_path = "/Users/liuchengyue/Desktop/BinarySCA Platform/Code/sca_agents/bsca-expert-agent-api/tmp/evaluation_reports/Conan/ours/gpt_5_mini/evaluation_report_reanalyzed.json"
-    Conan_only_failed_cases_evluation_report_path = "/Users/liuchengyue/Desktop/BinarySCA Platform/Code/sca_agents/bsca-expert-agent-api/tmp/evaluation_reports/Conan/ours/gpt_5_mini/only_failed_cases_evaluation_report.json"
-
-
-    benchmark_meta = Conan_benchmark_meta_updated
-    benchmark_tc_dir = Conan_test_case_dir
-    evaluation_report_save_path = Conan_only_failed_cases_evluation_report_path
-
-    # 评估配置
-    config = EvaluationConfig(
-        benchmark_file=benchmark_meta,
-        test_case_dir=benchmark_tc_dir,
-        feature_matching_top_n=5,
-        # use_agent=False,
-        concurrency=20,
-        slice_start=0,
-        # slice_end=0,
-        input_token_price_per_1M=0.4,
-        output_token_price_per_1M=1.6,
-    )
-
-    # 初始化评估器
-    evaluator = Evaluator(config)
-
-    # 评估
-    evaluator.run_failed_cases(reference_report_path=Conan_evluation_report_path)
-    evaluator.report.dump(evaluation_report_save_path)
-
-    # 重新分析结果
-    report = evaluator.reanalyze_report(evaluation_report_save_path,
-                                        ignore_failed_cases=False)
-
-
-def stable_test():
-    # benchmark meta
-    evaluation_dir = env.str("EVALUATION_DIR_PATH")
-    evaluation_output_dir = env.str("EVALUATION_OUTPUT_DIR_PATH")
-    conan_test_case_dir = env.str("CONAN_BENCHMARK_TEST_CASE_DIR")
-
-    conan_benchmark_meta_file = f"{evaluation_dir}/general_benchmarks/benchmark_meta/conan_library_benchmark_20250831_2032.json"
-
-    evaluation_result_dir= f"{evaluation_output_dir}/Conan/ours/gpt_5_nano_5_times"
-
-    # evaluation config
-    config = EvaluationConfig(
-        benchmark_file=conan_benchmark_meta_file,
-        test_case_dir=conan_test_case_dir,
-        feature_matching_top_n=5,
-        # use_agent=False,
-        concurrency=30,
-        slice_start=0,
-        # slice_end=10,
-        input_token_price_per_1M=0,
-        output_token_price_per_1M=0,
-        # input_token_price_per_1M=0.4,
-        # output_token_price_per_1M=1.6,
-    )
-
-    for i in range(1, 6):
-        print(f"================= Run the {i} time =================")
-        evaluation_result_file = f"{evaluation_result_dir}/evaluation_report_{i}.json"
-
-        # 初始化评估器
-        evaluator = Evaluator(config)
-
-        # 评估
-        evaluator.run_benchmark(analyze_context=False)
-        evaluator.report.dump(evaluation_result_file)
-
-        # 重新分析结果
-        report = evaluator.reanalyze_report(evaluation_result_file,
-                                            ignore_failed_cases=False)
-
-def print_stable_test():
-    results_dir = "/Users/liuchengyue/Desktop/BinarySCA Platform/Code/sca_agents/bsca-expert-agent-api/tmp/evaluation_reports/Conan/ours/gpt_5_nano_5_times"
-    reports = []
-    for file in os.listdir(results_dir):
-        if file.endswith(".json"):
-            result_path = os.path.join(results_dir, file)
-            with open(result_path, "r") as f:
-                simple_report = json.load(f)
-                simple_report = SimpleEvaluationReport.init_from_dict(simple_report)
-                reports.append(simple_report)
-
-    csv_lines = []
-    csv_header = f"Recall, Precision, F1-Score, TP, FP, FN"
-    for report in reports:
-        print(report.research_question_data.effectiveness)
-        effectiveness = report.research_question_data.effectiveness
-        csv_lines.append(f"{effectiveness.recall}, {effectiveness.precision}, {effectiveness.f1_score}, {effectiveness.tp_count}, {effectiveness.fp_count}, {effectiveness.fn_count}")
-
-    # 写入csv文件
-    csv_path = os.path.join(results_dir, "summary.csv")
-    with open(csv_path, "w") as f:
-        f.write(csv_header + "\n")
-        for line in csv_lines:
-            f.write(line + "\n")
 
 def main():
     # benchmark meta
@@ -245,10 +17,6 @@ def main():
 
     evaluation_result_file = f"{evaluation_output_dir}/Conan/ours/gpt_5_mini_no_COT/evaluation_report.json"
 
-    # car_150_benchmark_meta_file = f"{evaluation_dir}/general_benchmarks/benchmark_meta/CAR150.json"
-    # car_150_test_case_dir = "/Users/liuchengyue/Desktop/BinarySCA Platform/Data/Test_Cases/TPL_Test_Cases/BYD"
-    # car_150_report = "/Users/liuchengyue/Desktop/BinarySCA Platform/Code/sca_agents/bsca-expert-agent-api/tmp/evaluation_reports/CAR_150/evaluation_report.json"
-
     # evaluation config
     config = EvaluationConfig(
         benchmark_file=conan_benchmark_meta_file,
@@ -258,55 +26,19 @@ def main():
         concurrency=30,
         slice_start=0,
         # slice_end=10,
-        input_token_price_per_1M=0,
-        output_token_price_per_1M=0,
-        # input_token_price_per_1M=0.4,
-        # output_token_price_per_1M=1.6,
+        input_token_price_per_1M=0.4,
+        output_token_price_per_1M=1.6,
     )
 
-    # 初始化评估器
+    # evaluator
     evaluator = Evaluator(config)
 
-    # 评估
+    # run benchmark
     evaluator.run_benchmark(analyze_context=False)
+
+    # dump report
     evaluator.report.dump(evaluation_result_file)
-
-    # 重新分析结果
-    # report = evaluator.reanalyze_report(car_150_report, ignore_failed_cases=False)
-
-    # 可视化分析结果
-    # visualization_html = "/Users/liuchengyue/Desktop/BinarySCA Platform/Code/sca_agents/bsca-expert-agent-api/tmp/visualization.html"
-    # generate_analysis_report(report,
-    #                         visualization_html
-    #                          )
 
 
 if __name__ == '__main__':
-    # main()
-    # stable_test()
-    # print_stable_test()
-    # run_failed_case_only()
-    # reanalyze_all_result()
-    run_feature_matching_only()
-
-
-    """
-    常用命令：
-    查看后台进程，杀死后台进程
-        ps aux | grep pipeline_runner.py
-        pkill -f pipeline_runner.py & pkill -f rsync & rm -rf /dev/shm
-        pkill -f pipeline_runner.py
-        
-        pkill -9 -f git -u wenze
-    清空临时目录
-        rm -rf /dev/shm
-    
-    激活conda环境并运行
-        conda activate sca
-    
-    后台运行
-        nohup python3 evaluator_runner.py > evaluation.log 2>&1 &
-    
-    跟踪查看日志
-        tail -f evaluation.log
-    """
+    main()
